1736b51529e80b699a8be5b4101f2aff82b8647f
LFG-AAI <156384686+LFG-AAI@users.noreply.github.com>
2024-07-22 16:32:22 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,21 @@
-"""
+import py_compile
+from colorama import init, Fore, Style
+
+# Inisialisasi Colorama
+init(autoreset=True)
+
+def encrypt_and_rename_file(input_filename, output_filename):
+    try:
+        # Kompilasi file Python menjadi bytecode
+        compiled_filename = output_filename
+        py_compile.compile(input_filename, cfile=compiled_filename)
+        print(Fore.RED + f"File {input_filename} berhasil dienkripsi dan disimpan sebagai {compiled_filename}")
+
+    except Exception as e:
+        print(Fore.RED + f"Terjadi kesalahan saat mengenkripsi file {input_filename}: {str(e)}")
+
+if __name__ == "__main__":
+    print(Fore.YELLOW + """
 ===========================================================
   ______    ______   ______ 
  /      \  /      \ /      |
@@ -30,22 +47,7 @@ Masukkan nama file output yang diinginkan (contoh): myscript_encrypted
 File myscript.py berhasil dienkripsi dan disimpan sebagai myscript_encrypted.
 
 ===========================================================
-"""
-
-import py_compile
-
-def encrypt_and_rename_file(input_filename, output_filename):
-    try:
-        # Kompilasi file Python menjadi bytecode
-        compiled_filename = output_filename
-        py_compile.compile(input_filename, cfile=compiled_filename)
-        print(f"File {input_filename} berhasil dienkripsi dan disimpan sebagai {compiled_filename}")
-
-    except Exception as e:
-        print(f"Terjadi kesalahan saat mengenkripsi file {input_filename}: {str(e)}")
-
-if __name__ == "__main__":
-    print(__doc__)  # Menampilkan intro saat skrip dijalankan
+""")
 
     input_filename = input("Masukkan nama file yang ingin dienkripsi (contoh.py): ")
     output_filename = input("Masukkan nama file output yang diinginkan (contoh): ")
